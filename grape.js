a571067a2b97a9a66b4cf85aea5c81d4f6df88ff
DeyLynn <sosaminosa@gmail.com>
2022-05-10 21:49:00 -0400

    File color update

--- grape.js
+++ grape.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#2d283f", "#ed2261", "#ec7d7a","#e7702c", "#487df4", "#8d35c9", "#3bdeed", "#9e9ea0", "#59516a", "#f0729a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
+t.prefs_.set('color-palette-overrides',["#2d283f", "#ed2261", "#ec7d7a","#e7702c", "#487df4", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#f0729a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
 t.prefs_.set('foreground-color', "#9f9fa1");
 t.prefs_.set('background-color', "#17141f");
 t.prefs_.set('cursor-color', 'rgba(162,136,247,0.5)');
