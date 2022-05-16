daae254f4e4564bdc6a47ba368f99e58acac6c78
DeyLynn <sosaminosa@gmail.com>
2022-05-16 00:32:42 -0400

    Change theme colors

--- blueberry_banana.js
+++ blueberry_banana.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#17141f", "#ed2261", "#fd236a","#e7702c", "#a7aaec", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#f0729a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
-t.prefs_.set('foreground-color', "#5ae4fa");
-t.prefs_.set('background-color', "#17141f");
-t.prefs_.set('cursor-color', 'rgba(248,237,79,0.5)');
+t.prefs_.set('color-palette-overrides',["#2d283f", "#ed2261", "#1fa91b", "#8ddc20", "#487df4", "#8d35c9", "#3bdeed", "#9e9ea0", "#59516a", "#f0729a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
+t.prefs_.set('foreground-color', "#9f9fa1");
+t.prefs_.set('background-color', "#171423");
+t.prefs_.set('cursor-color', 'rgba(162,136,247,0.5)');
