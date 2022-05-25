dded8ebb62a15dd83c3953482fe3206e48a21cd6
DeyLynn <sosaminosa@gmail.com>
2022-05-25 17:15:48 -0400

    Change cfLocate to cf_locate

--- blueberry_banana.js
+++ blueberry_banana.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#17141f", "#fd236a", "#fd236a","#e7702c", "#a7aaec", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#fd236a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
+t.prefs_.set('color-palette-overrides',["#17141f", "#f0729a", "#f0729a","#e7702c", "#a7aaec", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#fd236a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
 t.prefs_.set('foreground-color', "#5ae4fa");
 t.prefs_.set('background-color', "#17141f");
 t.prefs_.set('cursor-color', 'rgba(248,237,79,0.5)');
