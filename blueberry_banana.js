0e2e1cf7ec92c41daa0a92390071d2339b67c159
DeyLynn <sosaminosa@gmail.com>
2022-05-25 17:44:34 -0400

    File color update

--- blueberry_banana.js
+++ blueberry_banana.js
@@ -1,5 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#17141f", "#ec7d7a", "#ec7d7a", "#e7702c", "#a7aaec", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#ec7d7a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
+t.prefs_.set('color-palette-overrides',["#17141f", "#ed2261", "#fd236a","#e7702c", "#a7aaec", "#8f6aa8", "#3bdeed", "#9e9ea0", "#59516a", "#f0729a", "#53aa5e", "#b2dc87", "#a9bcec", "#ad81c2", "#9de3eb", "#a288f7"]);
 t.prefs_.set('foreground-color', "#5ae4fa");
 t.prefs_.set('background-color', "#17141f");
 t.prefs_.set('cursor-color', 'rgba(248,237,79,0.5)');
-
