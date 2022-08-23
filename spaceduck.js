2dad0154f00542ede4b2912cbf6ad6719a67ed96
DeyLynn <sosaminosa@gmail.com>
2022-08-22 23:20:22 -0400

    Add SpaceDuck color theme

---
+++ spaceduck.js
@@ -0,0 +1,27 @@
+black = "#000000";
+red = "#e33500";
+green = "#5ccc96";
+yellow = "#b3a1e6";
+blue = "#00a3cc";
+magenta = "#ce6f8f";
+cyan = "#7a5ccc";
+white = "#686f98";
+lightBlack = "#686f9a";
+lightRed = "#e33500";
+lightGreen = "#5ccc96";
+lightYellow = "#b3a1e6";
+lightBlue = "#00a3cc";
+lightMagenta = "#ce6f8f";
+lightCyan = "#7a5ccc";
+lightWhite = "#ecf0c1";
+
+t.prefs_.set('color-palette-overrides',
+                [ black, red, green, yellow,
+                  blue, magenta, cyan, white,
+                  lightBlack, lightRed, lightGreen, lightYellow,
+                  lightBlue, lightMagenta, lightCyan, lightWhite ]);
+
+t.prefs_.set('cursor-color', '#5ae4fa');
+t.prefs_.set('foreground-color', '#ecf0c1');
+t.prefs_.set('background-color', '#0f111b');
+
